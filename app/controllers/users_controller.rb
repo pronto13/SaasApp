c40a3fb989ad1c471f43cc98e409d8a1b9e17255
Mar S <marasi.ae@gmail.com>
2017-11-02 18:15:30 +0000

    Added users index view and action details

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,10 @@ class UsersController < ApplicationController
   before_action :authenticate_user!
   
   def index
-    
+    # Combine user table into one query as opposed to all. individual query (heavy on the server)
+    # Check log to compare request between includes and all
+    # also check out sql join - active record querying
+    @users = User.includes(:profile)
   end 
   
   # GET to /users/:id
