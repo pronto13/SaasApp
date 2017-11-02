eb21e7d633e7f16733a2359f8758e2f76f32d8a1
Mar S <marasi.ae@gmail.com>
2017-11-02 14:45:25 +0000

    Fixed some errors in profile display

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,10 +1,11 @@
 # You can put all sort of misc function here
 # can inject instance variable from controllers
+
 module UsersHelper
   def job_title_icon
     if @user.profile.job_title == "Developer"
-      "<i class='fa fa-code></i>".html_safe
-    elsif @user.profile.job_title == "Etrepreneur"
+      "<i class ='fa fa-code'></i>".html_safe
+    elsif @user.profile.job_title == "Entrepreneur"
       "<i class ='fa fa-lightbulb-o'></i>".html_safe
     elsif @user.profile.job_title == "Investor"
       "<i class = 'fa fa-dollar'></i>".html_safe
