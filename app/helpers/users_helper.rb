d175610855cc2f83097d7ac11059425f64ab9b0f
Mar S <marasi.ae@gmail.com>
2017-11-02 19:35:23 +0000

    Added helpful copy

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -3,11 +3,11 @@
 
 module UsersHelper
   def job_title_icon
-    if @user.profile.job_title == "Developer"
-      "<i class ='fa fa-code'></i>".html_safe
-    elsif @user.profile.job_title == "Entrepreneur"
-      "<i class ='fa fa-lightbulb-o'></i>".html_safe
-    elsif @user.profile.job_title == "Investor"
+    if @user.profile.job_title == "Bum"
+      "<i class ='fa fa-child'></i>".html_safe
+    elsif @user.profile.job_title == "Hobo"
+      "<i class ='fa fa-child'></i>".html_safe
+    elsif @user.profile.job_title == "Rich People"
       "<i class = 'fa fa-dollar'></i>".html_safe
     end  
   end
