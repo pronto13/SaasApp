88021fb193444cd98b5da0730b0561344279f14e
Mar S <marasi.ae@gmail.com>
2017-11-03 15:50:02 +0000

    Simplify

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -3,11 +3,11 @@
 
 module UsersHelper
   def job_title_icon
-    if @user.profile.job_title == "Bum"
-      "<i class ='fa fa-child'></i>".html_safe
-    elsif @user.profile.job_title == "Hobo"
-      "<i class ='fa fa-child'></i>".html_safe
-    elsif @user.profile.job_title == "Rich People"
+    if @user.profile.job_title == "Job1"
+      "<i class ='fa fa-dollar'></i>".html_safe
+    elsif @user.profile.job_title == "Job2"
+      "<i class ='fa fa-dollar'></i>".html_safe
+    elsif @user.profile.job_title == "Job3"
       "<i class = 'fa fa-dollar'></i>".html_safe
     end  
   end
